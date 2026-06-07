26ea3b186652274cd6a3929eed7f5ea6df0c06c6
Ieva Grauduma <ieva.grauduma@idgard.de>
2026-06-08 00:51:04 +0200

    add path

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+Path("./data").mkdir(exist_ok=True)
 engine = create_engine("sqlite:///./data/purchases.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
